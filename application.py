4cc40771586ecd4937e81183a43f86b70db06663
Qzwini <qzwini@gmail.com>
2020-05-24 21:41:16 +0300

    updated

--- application.py
+++ application.py
@@ -1,12 +1,19 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
+from wtform_fields import *
+
+
+
+# Configure app
 app = Flask(__name__)
 app.secret_key = 'replace later'
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    
+    reg_form = RegistrationForm()
 
-    return render_template("index.html")
+    return render_template("index.html", form=reg_form)
 
 if __name__ == '__main__':
 
---
+++ wtform_fields.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, EqualTo
+
+class RegistrationForm(FlaskForm):
+    """ Registration Form """
+
+    username = StringField('username_label')
+    password = PasswordField('password_label')
+    confirm_pswd = PasswordField('confirm_psw_label')
