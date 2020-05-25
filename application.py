62420acd356a2745af764a8b53996d459bfe74f1
Qzwini <qzwini@gmail.com>
2020-05-25 16:54:11 +0300

    connect to DB

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 from wtform_fields import *
+from  models import *
 
 
 
@@ -8,15 +9,33 @@ from wtform_fields import *
 app = Flask(__name__)
 app.secret_key = 'replace later'
 
+# Configure database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'postgres://itwpbnteihaupg:1a1204f0e914139fee598a3830ccbdc5fbec7bec0bb0dcb0120c36d002b04dcd@ec2-52-44-166-58.compute-1.amazonaws.com:5432/d39ct4ok2dqku6'
+db = SQLAlchemy(app)
+
 @app.route("/",methods=['GET','POST'])
 def index():
     
     reg_form = RegistrationForm()
+
     if reg_form.validate_on_submit():
-        return "Great success!"
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        # Check username exists
+        user_object = User.query.filter_by(username=username).first()
+        if user_object:
+            return "Someone else has taken this username!"
+
+        # Add username 
+        user = User(username=username, Password=Password)
+        db.session.add(user)
+        db.session.commit()
+        return "Inserted into db"
+
 
     return render_template("index.html", form=reg_form)
 
 if __name__ == '__main__':
-
+ 
     app.run(debug=True)
